3ef73e4beb1ac885926c5f2a8ba5c3c87fc299f1
YASWANT KUMAR SINGHI <40240984+Yaswant-Kumar-Singhi@users.noreply.github.com>
2019-08-27 23:35:21 +0530

    Delete jpg2png.py

--- custom_plate/jpg2png.py
+++
@@ -1,7 +0,0 @@
-from glob import glob                                                           
-import cv2 
-jpgs = glob('./*.JPG')
-
-for j in jpgs:
-    img = cv2.imread(j)
-    cv2.imwrite(j[:-3] + 'png', img)
